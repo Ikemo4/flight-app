0d03592a2cfa5a99055d9379e0092eb899edf2d3
Isaac Morris <isaacmorris@Isaacs-MacBook-Pro.local>
2022-05-04 11:44:56 -0600

    more flight detail component. about to add parameterized routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { FlightListComponent } from './flight-list/flight-list.component';
+import { FlightDetailComponent } from './flight-detail/flight-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'flights', pathMatch: 'full' },
-  { path: 'flights', component: FlightListComponent }
+  { path: 'flights', component: FlightListComponent },
+  { path: 'detail/:id', component: FlightDetailComponent }
 ];
 
 @NgModule({
--- src/app/flight-detail/flight-detail.component.ts
+++ src/app/flight-detail/flight-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Flight } from '../flight.model';
 
 @Component({
   selector: 'app-flight-detail',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight-detail.component.css']
 })
 export class FlightDetailComponent implements OnInit {
+
+  @Input() flight?: Flight;
 
   constructor() { }
 
